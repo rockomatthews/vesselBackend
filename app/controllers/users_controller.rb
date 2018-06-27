3abd13c9520e056133e314b8fb206a607ec12826
Rob Matthews <rob.matthews@me.com>
2018-06-27 10:09:31 -0500

    connected to conduit api.  Still a few errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-    before_action :authenticate_user!
+  before_action :authenticate_user!
+
+  def show
   end
 
-def show
-end
-    
-def update
+  def update
     if current_user.update_attributes(user_params)
-        render :show
+      render :show
     else
-        render json: { errors: current_user.errors }, status: :unprocessable_entity
+      render json: { errors: current_user.errors }, status: :unprocessable_entity
     end
-end
+  end
 
-private
+  private
 
-def user_params
-params.require(:user).permit(:username, :email, :password, :bio, :image)
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :bio, :image)
+  end
 end
